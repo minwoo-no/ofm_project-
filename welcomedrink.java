c2b7455026ce3ac7d63eac44e2195618b1d90d33
mwno <nnohmin@gmail.com>
2022-07-05 17:25:40 +0900

    mwno class 1

--- welcomedrink.java
+++ welcomedrink.java
@@ -1,5 +1,11 @@
 public class welcomedrink {
 
     // mwno class
+    public static void main(String[] args) {
+    System.out.println("####################################");
+    System.out.println("Sample Code ");
+    System.out.println("####################################");
 
+    }
 }
+
